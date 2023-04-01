a65053b4590fab41a1f17fca80cea7856d6e5afc
sindongha <tlsehdgk87@naver.com>
2023-04-01 21:44:47 +0900

    졸려

---
+++ src/Test/구구단.java
@@ -0,0 +1,28 @@
+package Test;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class 구구단 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        for (int i = 1; i <= 9; i++) {
+            System.out.println(n+" * "+ i+" = "+i*n);
+        }
+
+
+
+
+
+
+
+
+
+    }
+}
